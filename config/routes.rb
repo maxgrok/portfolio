6edd71e25e48a2fc9596f90101a3747b496562a2
codecademy <ccuser@codecademy.com>
2016-07-17 22:34:00 -0400

    Generating controller for About

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get '/images' => 'images#home'
+  get '/about' =>
+    'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
